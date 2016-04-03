1cd9f7e73a91b73edddb114c2bfed04201976ed6
smartdeepan <smartdeepan@gmail.com>
2016-04-04 01:31:12 +0530

    Update Pick up drop escape.c

--- Pick up drop escape.c
+++ Pick up drop escape.c
@@ -4,45 +4,45 @@ int n;
 int a[10000000];
 int globalxor=-65553636;
 int initk;
-int fun(int i,int k,int lxor){
+int fun(int i,int k,int lxor)
+{
 
 
-	if(k==0){
-        if(globalxor<lxor){
-                globalxor=lxor;
-                //printf("ans  ",lxor);
-               //printf("\n");
+    if(k==0)
+    {
+        if(globalxor<lxor)
+        {
+            globalxor=lxor;
 
         }
 
         k=initk;
         lxor=-65553636;
 
-}
+    }
 
-if(i>=n){
-    return;
-}
-
-
-
+    if(i>=n)
+    {
+        return;
+    }
 
 
 
-//printf("goto i=%d , k=%d , lxor=%d  ",i+1,k-1,lxor);
-//printf("\n");
-fun(i+1,k,lxor);
 
-if(lxor==-65553636){
-    lxor=a[i];
-}
-else{
-    lxor=lxor^a[i];
-}
-//printf("goto i=%d , k=%d , lxor=%d  ",i+1,k-1,lxor);
-//printf("\n");
 
-fun(i+1,k-1,lxor);
+
+    fun(i+1,k,lxor);
+
+    if(lxor==-65553636)
+    {
+        lxor=a[i];
+    }
+    else
+    {
+        lxor=lxor^a[i];
+    }
+
+    fun(i+1,k-1,lxor);
 
 }
 
@@ -55,21 +55,23 @@ int main()
 
 
     scanf("%d",&t);
-    while(t--){
-    scanf("%d",&n);
-    scanf("%d",&k);
-    for(i=0;i<n;i++){
-         scanf("%d",&a[i]);
-    }
+    while(t--)
+    {
+        scanf("%d",&n);
+        scanf("%d",&k);
+        for(i=0; i<n; i++)
+        {
+            scanf("%d",&a[i]);
+        }
 
-    int lxor=-65553636;
+        int lxor=-65553636;
 
- globalxor=-65553636;
-    initk=k;
-   fun(0,k,lxor);
+        globalxor=-65553636;
+        initk=k;
+        fun(0,k,lxor);
 
 
-	  printf("%d\n",globalxor);
+        printf("%d\n",globalxor);
     }
 
     return 0;
